c20a8a687d57e70c56a09af0c741b394783d0b45
GentianVeliu <44606115+GentianVeliu@users.noreply.github.com>
2019-05-01 10:59:52 +0200

    projekti

---
+++ TripleDES/TripleDES/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace TripleDES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button btnEnkripto;
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            btnEnkripto = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEnkripto
+            // 
+            btnEnkripto.Location = new System.Drawing.Point(116, 96);
+            btnEnkripto.Name = "btnEnkripto";
+            btnEnkripto.Size = new System.Drawing.Size(145, 48);
+            btnEnkripto.TabIndex = 0;
+            btnEnkripto.Text = "Enkripto";
+            btnEnkripto.UseVisualStyleBackColor = true;
+            btnEnkripto.Click += new System.EventHandler(this.btnEnkripto_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Teksti";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(393, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(116, 171);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(393, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enkriptimi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 380);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(btnEnkripto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TripleDES/TripleDES/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TripleDES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnkripto_Click(object sender, EventArgs e)
+        {
+            string key = "Projekti Ne Siguri";
+            // Create a new instance of the MD5CryptoServiceProvider object.
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            // Convert the input string to a byte array and compute the hash.
+            byte[] keyArr = md5.ComputeHash(Encoding.UTF8.GetBytes(key));
+            md5.Clear();
+
+            byte[] TekstiTxt = Encoding.UTF8.GetBytes(textBox1.Text);
+
+            TripleDESCryptoServiceProvider des3 = new TripleDESCryptoServiceProvider();
+            des3.Key = keyArr;
+            des3.Mode = CipherMode.ECB;
+            des3.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform iTransform = des3.CreateEncryptor();
+            byte[] rezultati = iTransform.TransformFinalBlock(TekstiTxt, 0, TekstiTxt.Length);
+            textBox2.Text = Convert.ToBase64String(rezultati);
+
+        }
+    }
+}
